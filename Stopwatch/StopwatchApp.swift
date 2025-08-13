1d54ecc0fc6774e492ff308bf683b4294a9c11e8
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-08-13 12:28:58 +0300

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Анастасія Грисюк on 13.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchTests.swift
+//  StopwatchTests
+//
+//  Created by Анастасія Грисюк on 13.08.2025.
+//
+
+import Testing
+@testable import Stopwatch
+
+struct StopwatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
